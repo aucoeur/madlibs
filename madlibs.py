d78f6f356b372a8ecab8d90bd55fffe0a71076dc
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-08-30 11:04:52 -0700

    Set up preliminary list dict and noun prompt

--- madlibs.py
+++ madlibs.py
@@ -5,7 +5,23 @@
 # I've been here for [LOW NUMBER] days.  Today I woke up with [LOW NUMBER] [INSECT] bites.  It's been [ADJECTIVE].  I love the [ADJECTIVE] weather here but the [NOUN] leaves much to be desired.  School is [ADVERB] ramping up.  I'm [ADJECTIVE]. My family is eager to visit soon.  I hope [VERB-ING] won't be too much of an issue! 
 # Love, [NAME]
 
-def begin_game():
+part_of_speech_dict = {
+    "noun": "a person, place, or thing. Example: book",
+    "verb": "used to describe an action, state, or occurence. Example: swim",
+    "adjective": "used to describe or modify nouns or pronouns. Example: gorgeous",
+    "adverb": "modifies or qualifies an adjective, verb or other adverb. Example: quickly"
+}
+
+word_dict = {
+    'nouns': [], 'verbs': [], 'adjectives': [], 'adverbs': []}
+# Adding words to lists
+def add_noun():
+    user_noun = input('Enter a noun: ')
+    word_dict['nouns'].append(user_noun)
+    # word_dict['nouns'][0] returns 'Book' | dict['key'][list_index]
+    # word_dict.items() returns dict items
+
+def init_prompt():
     start = input('Hello ' + player_name + '. Ready to play? ')
     if start == 'yes':
         print('OKAY')
@@ -13,8 +29,12 @@ def begin_game():
         print('GOODBYE')
     else:
         print('please enter yes or no')
-        begin_game()
+        init_prompt()
+
+#def begin_game(): 
+
+# def pos_help():
 
 player_name = input('Hello there. What\'s your name? ')
 
-begin_game()
+init_prompt()
