86f6ec455798d880484951b24958f5cc29da9336
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-09-01 21:03:04 -0700

    Add check for integers

--- madlibs.py
+++ madlibs.py
@@ -3,12 +3,12 @@
 from string import Template
 
 # List of keys
-key_list = ["low_num", "num", "bug", "animal", "adj", "adj2", "adj3", "noun", "noun2", "adverb", "verb", "verb2"]
+key_list = ["num1", "num2", "bug", "animal", "adj", "adj2", "adj3", "noun", "noun2", "adverb", "verb", "verb2"]
 
 # List of keys and prompt phrasing
 key_name = {
-    'low_num': 'a number between 1-10',
-    'num': 'a number',
+    'num1': 'a number',
+    'num2': 'a number',
     'bug': 'a bug',
     'animal': 'an animal',
     'adj': 'an adjective',
@@ -21,13 +21,28 @@ key_name = {
     'verb2': 'a verb ending in "-ing"'}
 
 # Initialize word dictionary for input words
-input_words = dict.fromkeys(key_list, None)
+# input_words = dict.fromkeys(key_list, None)
+input_words = {}
 
 # Ask for user inputs
 def add_words():
     for word_type, name in key_name.items():
         value = input('Enter ' + str(name) + ": ")
-        input_words[word_type] = text_format(value)
+        if word_type == 'num1' or word_type == 'num2':
+            check_int(word_type, value)    
+        else:
+            input_words[word_type] = text_format(value)
+
+# Check if integer
+def check_int(word_type, value):
+    while True:
+        if (value.isdigit()):
+            input_words[word_type] = text_format(value)
+            break
+        else:
+            key = input_words.get(word_type)
+            input_words.pop(key, value)
+            value = input('Input was string. Enter a number: ')
 
 # Set input words to MAGENTA and BOLD
 def text_format(value):
@@ -47,7 +62,7 @@ def init_prompt():
         print('\n' + player_name.upper() + "\'S MAD LIB")
         print('-----------------------------')
         # Mad Lib story template
-        story = Template('Dear Diary,\n\nIt\'s me, ' + player_name + '.  I\'ve been here for $low_num days.  Today I woke up with $num $bug bites.  At least, it\'s not $animal bites.\n\nIt\'s been $adj.  I love the $adj2 weather here but the $noun leaves much to be desired.  School is $adverb ramping up.\n\nI\'m $adj3. My $noun2 is eager to $verb soon.  I hope $verb2 won\'t be too much of an issue!\n\nLove, ' + player_name)
+        story = Template('Dear Diary,\n\nIt\'s me, ' + player_name + '.  I\'ve been here for $num1 days.  Today I woke up with $num2 $bug bites.  At least, it\'s not $animal bites.\n\nIt\'s been $adj.  I love the $adj2 weather here but the $noun leaves much to be desired.  School is $adverb ramping up.\n\nI\'m $adj3. My $noun2 is eager to $verb soon.  I hope $verb2 won\'t be too much of an issue!\n\nLove, \n' + player_name)
 
         print(story.substitute(input_words))
     elif start == 'no' or start == 'n':
